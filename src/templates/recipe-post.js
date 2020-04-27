29375113e80ca3f644b38465adaba888679bd76b
Catia Costa <NORBERTO.CATIA@MacBook-Pro-de-Catia.local>
2020-04-27 23:57:50 +0100

    Revert "[recipes] - Try to fix procedure field."
    This reverts commit c827eb983dec9fd96f22e2353f81300be9f1cc91.

--- src/templates/recipe-post.js
+++ src/templates/recipe-post.js
@@ -261,6 +261,7 @@ export const RecipePostTemplate = ({
   difficulty,
   time, 
   Ingredients,
+  Procedure,
   meal_type,
   servings,
   serving_hea,
@@ -316,10 +317,10 @@ export const RecipePostTemplate = ({
               <PostContent content={content} />
               <RecipeIngredients>
                 {Ingredients}
+                <PostContent content={content} />
               </RecipeIngredients>
               <RecipeProcedure>
-                <PostContent content={content} />
-   
+                {Procedure}
               </RecipeProcedure>
             </RecipeRow>
             <RecipeRow>
@@ -458,7 +459,7 @@ const RecipePost = ({ data }) => {
         time={post.frontmatter.time}
         difficulty ={post.frontmatter.difficulty}
         Ingredients={post.frontmatter.Ingredients}
-        //Procedure={post.frontmatter.Procedure}
+        Procedure={post.frontmatter.Procedure}
         meal_type={post.frontmatter.meal_type}
         servings={post.frontmatter.servings}
         serving_hea={post.frontmatter.serving_hea}
@@ -499,6 +500,7 @@ export const pageQuery = graphql`
         difficulty
         time
         Ingredients
+        Procedure
         tags
         meal_type
         servings
