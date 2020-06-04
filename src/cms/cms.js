3c9b4f9878da8471d2a84a63d09d0de2de205ac5
Catia Costa <NORBERTO.CATIA@MacBook-Pro-de-Catia.local>
2020-06-05 00:22:36 +0100

    Revert "[preview] - Fix design on preview."
    This reverts commit 4551c5eef99311b082d28284b7c2da672ca09b52.

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -7,38 +7,10 @@ import RecipePostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
-import React, { useState, useEffect } from 'react';
-import { StyleSheetManager } from 'styled-components';
-
-function StyleInjector({ children }) {
-  const [iframeRef, setIframeRef] = useState(null);
-
-  useEffect(() => {
-    const iframe = document.getElementsByTagName('iframe')[0];
-    const iframeHeadElem = iframe.contentDocument.head;
-    setIframeRef(iframeHeadElem);
-  }, []);
-
-  return (
-    iframeRef && (
-      <StyleSheetManager target={iframeRef}>{children}</StyleSheetManager>
-    )
-  );
-}
-
-export default function withStyledComponentsRendered(Comp) {
-  return props => (
-    <StyleInjector>
-      <Comp {...props} />
-    </StyleInjector>
-  );
-}
-
-
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
-CMS.registerPreviewTemplate('index', withStyledComponentsRendered(IndexPagePreview))
-CMS.registerPreviewTemplate('about', withStyledComponentsRendered(AboutPagePreview))
-CMS.registerPreviewTemplate('products', withStyledComponentsRendered(ProductPagePreview))
-CMS.registerPreviewTemplate('recipe', withStyledComponentsRendered(RecipePostPreview))
+CMS.registerPreviewTemplate('index', IndexPagePreview)
+CMS.registerPreviewTemplate('about', AboutPagePreview)
+CMS.registerPreviewTemplate('products', ProductPagePreview)
+CMS.registerPreviewTemplate('recipe', RecipePostPreview)
