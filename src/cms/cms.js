9a82373f578f1780c04da07a85d9d4fe7d87fda4
Catia Costa <NORBERTO.CATIA@MacBook-Pro-de-Catia.local>
2020-06-05 00:37:43 +0100

    [preview] - Another try to fix recipes preview.

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,3 +1,6 @@
+import React from 'react'
+import StyledSheets from "./styleSheetManager"
+
 import CMS from 'netlify-cms-app'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
@@ -7,10 +10,17 @@ import RecipePostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
+
+
+
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('recipe', RecipePostPreview)
+CMS.registerPreviewTemplate('recipe', props => (
+    <StyledSheets>
+      <RecipePostPreview {...props} />
+    </StyledSheets>
+))
