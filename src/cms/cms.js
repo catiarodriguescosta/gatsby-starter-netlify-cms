9d028d50af65050f33e10ad55d285044eed79bbc
Catia Costa <NORBERTO.CATIA@MacBook-Pro-de-Catia.local>
2020-06-05 01:29:23 +0100

    Revert "[preview] - Another try to fix recipes preview."
    This reverts commit 9a82373f578f1780c04da07a85d9d4fe7d87fda4.

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,3 @@
-import React from 'react'
-import StyledSheets from "./styleSheetManager"
-
 import CMS from 'netlify-cms-app'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
@@ -10,17 +7,10 @@ import RecipePostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
-
-
-
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('recipe', props => (
-    <StyledSheets>
-      <RecipePostPreview {...props} />
-    </StyledSheets>
-))
+CMS.registerPreviewTemplate('recipe', RecipePostPreview)
